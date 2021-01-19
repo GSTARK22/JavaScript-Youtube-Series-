7509e4c9a5e99e65744e20c6d8c51433111d48be
GSTARK22 <72339595+GSTARK22@users.noreply.github.com>
2021-01-19 20:17:23 +0530

    episode1.js
    Nothing the source code for the first episode of my youtube series

---
+++ Episode 1/episode1.js
@@ -0,0 +1,3 @@
+alert('Hello World') //Displays A Notification or a pop up in your browser syaing hello world
+
+console.log('Hello World') //Prints Hello World In your Terminal, CMD, Screen or Console.
